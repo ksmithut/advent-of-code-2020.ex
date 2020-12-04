830619e8bde65fff4c6c02d2d68228176f7c3db1
ksmithut <github@ksmithut.com>
2020-12-04 07:15:19 -0700

    day 4

--- test/advent_of_code_test.exs
+++ test/advent_of_code_test.exs
@@ -4,7 +4,7 @@ defmodule AdventOfCodeTest do
   doctest AdventOfCode.Day01
   doctest AdventOfCode.Day02
   doctest AdventOfCode.Day03
-  # doctest AdventOfCode.Day04
+  doctest AdventOfCode.Day04
   # doctest AdventOfCode.Day05
   # doctest AdventOfCode.Day06
   # doctest AdventOfCode.Day07
